09ddeabf92bc3986959db650f83c5f4ca35230a3
Whisu <m3szuge@o2.pl>
2017-07-01 10:27:04 +0200

    Tests

--- src/test/java/kolokwium/a/pl/edu/us/awpto/FizzBuzzTest.java
+++ src/test/java/kolokwium/a/pl/edu/us/awpto/FizzBuzzTest.java
@@ -2,8 +2,42 @@ package kolokwium.a.pl.edu.us.awpto;
 
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import java.util.Arrays;
+import java.util.Collection;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
 public class FizzBuzzTest
 {
+	private int number;
+	private String result;
+	private FizzBuzz fizzBuzz;
+	
+	public FizzBuzzTest(int testParameter, String result) {
+		number = testParameter;
+		this.result = result;
+	}
+	
+	@Parameters
+	public static Collection<Object[]> data() {
+		Object[][] data = new Object[][] { { 15, "FizzBuzz" }, { 6, "Fizz" }, { 10, "Buzz" }, { 7, "7"} };
+		return Arrays.asList(data);
+	}
+	
+	@Before
+	public void setUp() {
+		fizzBuzz = new FizzBuzz();
+	}
+	
+	@Test
+	public void testCalc() {
+		String currentResult = fizzBuzz.calculate(number);
+		
+		assertEquals(currentResult, result);
+	}
 }
--- src/test/java/kolokwium/a/pl/edu/us/awpto/NumberPrinterTest.java
+++ src/test/java/kolokwium/a/pl/edu/us/awpto/NumberPrinterTest.java
@@ -1,9 +1,20 @@
 package kolokwium.a.pl.edu.us.awpto;
 
-import static org.junit.Assert.*;
-
+import org.junit.Before;
 import org.junit.Test;
 
 public class NumberPrinterTest
 {
+	private NumberPrinter np;
+	
+	@Before
+	public void setUp() {
+		np = new NumberPrinter(new FizzBuzz(), new ConsolePrinter());
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void testException() {
+		np.printNumbers(0);
+	}
+	
 }
